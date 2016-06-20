f6199c0ebdeeff65458b01b6c1f7f552f3d2cfb0
GrandpaGhostTim <guitaristintheforest@gmail.com>
2016-06-19 20:22:25 -0700

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |  XX  |  XX  |  XX  |  XX  |  XX  |  XX  |   -  |   =  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |BL Tog|BL Dwn|BL Up |  XX  |  XX  |  XX  |   _  |   +  |  XX  |   )  |      |
+ * |      |BL Tog|BL Dwn|BL Up | CADEL|  XX  |  XX  |   _  |   +  |  XX  |  XX  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      | End  | PgDwn| PgUp | Home |
  * `-----------------------------------------------------------------------------------'
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = {
   {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_BSPC},
   {_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE},
-  {_______, BL_TOGG, BL_DEC,  BL_INC,  CADEL,   XXXXXXX, XXXXXXX, KC_UNDS, KC_PLUS, XXXXXXX, KC_RPRN, _______},
+  {_______, BL_TOGG, BL_DEC,  BL_INC,  CADEL,   XXXXXXX, XXXXXXX, KC_UNDS, KC_PLUS, XXXXXXX, XXXXXXX, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN, KC_PGUP, KC_MPLY}
 },
 
@@ -135,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL},
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS},
   {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_PSCR, XXXXXXX, KC_MPRV, KC_MNXT, _______},
-  {KC_TRNS, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY}
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY}
 },
 
 
